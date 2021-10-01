eac15379cebd8f4729a9b22a8c18e90ebef41d04
hyuns66 <gustjr0606@naver.com>
2021-10-01 23:25:42 +0900

    no message

--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.example.flo.databinding.ActivityMainBinding
 
 
