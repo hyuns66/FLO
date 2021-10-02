eeea51db59d1bd07d3ec61d1d9e4a53330cd83d7
hyuns66 <gustjr0606@naver.com>
2021-10-02 21:03:15 +0900

    개빡쳐

--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -7,9 +7,11 @@ import com.example.flo.databinding.ActivitySongBinding
 import java.util.zip.Inflater
 
 class SongActivity : AppCompatActivity(){
+
     lateinit var binding : ActivitySongBinding
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
