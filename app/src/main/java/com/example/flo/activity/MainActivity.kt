ac9b2e89178cd118612276936fb059e637e7608e
hyuns66 <gustjr0606@naver.com>
2021-10-20 23:12:41 +0900

    splash

--- app/src/main/java/com/example/flo/activity/MainActivity.kt
+++ app/src/main/java/com/example/flo/activity/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     var backPressedTime : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/example/flo/activity/SplashActivity.kt
+++ app/src/main/java/com/example/flo/activity/SplashActivity.kt
@@ -3,15 +3,23 @@ package com.example.flo.activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.example.flo.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(){
+
+    lateinit var binding : ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
     }
 }
